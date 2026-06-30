3b95ed65ec5f902fa2eac46fa5926f70bb6ea96e
kiki chang <inbox@kiki.directory>
2026-06-30 16:23:57 -0400

    remove testing script

--- src/script.js
+++
@@ -1,5 +0,0 @@
-// on load, check the ids of what's in localStorage. If there are things that do not match the id's, remove them.
-
-// important js should be in head
-
-localStorage.setItem(notebookID, `/assets/${notebookID}/001.png`)
