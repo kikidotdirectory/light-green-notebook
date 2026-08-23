31780ec5ee621ef8624ffd4c991ab7c8f4c5f6d4
kiki chang <inbox@kiki.directory>
2026-08-23 17:04:28 -0400

    move lgn to index, flatten structure of assets

--- src/js/page-navigation.ts
+++ src/js/page-navigation.ts
@@ -1,47 +1,42 @@
-declare const assetsDir: string;
-declare const fileExt: string;
-declare const notebookID: string;
 declare const totalSpreads: number;
-declare function fileName(pageNum: number): string;
-
-interface NotebookState {
-	pageNum: number;
-}
 
 let currentPageNum: number;
-const prev = document.querySelector(".page-link.prev") as HTMLButtonElement;
-const nextButtons = document.querySelectorAll(".page-link.next") as NodeListOf<HTMLButtonElement>;
-const spreadImage = document.querySelector(".spread-image") as HTMLImageElement;
-const notebookViewer = document.querySelector(".notebook-viewer") as HTMLElement;
 
-prev?.addEventListener("click", () => toPage(-1));
-nextButtons.forEach((next) => next.addEventListener("click", () => toPage(1)));
+const fileName = (page: number) => String(page).padStart(4, "0");
 
-function getState() {
+function getState(): number {
 	try {
-		const raw = localStorage.getItem(notebookID);
-		return raw ? JSON.parse(raw) : { pageNum: 0 };
+		const raw = Number(localStorage.getItem(notebookID));
+		return raw < totalSpreads ? raw : 0;
 	} catch {
-		return { pageNum: 0 };
+		return 0;
 	}
 }
 
-function setState(partial: NotebookState) {
-	const updated = { ...getState(), ...partial };
-	localStorage.setItem(notebookID, JSON.stringify(updated));
+function setState(pageNum: number) {
+	localStorage.setItem(notebookID, pageNum.toString());
 }
 
 function getPage() {
-	currentPageNum = getState().pageNum;
-	spreadImage.src = imgSrc(currentPageNum);
+	currentPageNum = getState();
+	replaceSpreadImage(currentPageNum);
 	notebookViewer.dataset.spread = currentPageNum.toString();
 }
 
-function imgSrc(pageNum: number) {
-	return assetsDir + fileName(pageNum) + "." + fileExt;
+function numToPageSrc(pageNum: number) {
+	return "/assets/spreads/" + fileName(pageNum) + "." + fileExt;
 }
 
-function toPage(delta: number) {
+function replaceSpreadImage(pageNum: number) {
+	const newImage = document.createElement("img");
+	newImage.className = spreadImage.className;
+	newImage.alt = spreadImage.alt;
+	newImage.src = numToPageSrc(pageNum);
+	spreadImage.replaceWith(newImage);
+	spreadImage = newImage;
+}
+
+function incrementPage(delta: number) {
 	const dest = currentPageNum + delta;
 	// prevent navigation from going outside bounds of notebook
 	if (dest < 0 || dest > totalSpreads) return;
@@ -49,8 +44,8 @@ function toPage(delta: number) {
 	// change the page
 	currentPageNum = dest;
 	notebookViewer.dataset.spread = currentPageNum.toString();
-	spreadImage.src = imgSrc(currentPageNum);
-	setState({ pageNum: currentPageNum });
+	replaceSpreadImage(currentPageNum);
+	setState(currentPageNum);
 
 	// preload the next page in the current direction
 	if (currentPageNum !== 0 && currentPageNum !== totalSpreads) {
@@ -60,8 +55,18 @@ function toPage(delta: number) {
 
 function preloadPage(pageNum: number) {
 	const img = new Image();
-	img.src = imgSrc(pageNum);
+	img.src = numToPageSrc(pageNum);
 }
 
 // On page load, render notebook.
-getPage();
+const fileExt = "png";
+const notebookID = "lgn";
+
+const notebookViewer = document.querySelector(".notebook-viewer") as HTMLElement;
+const prev = document.querySelector(".page-link.prev") as HTMLButtonElement;
+const next = document.querySelector(".page-link.next") as HTMLButtonElement;
+let spreadImage = document.querySelector(".spread-image") as HTMLImageElement;
+
+prev?.addEventListener("click", () => incrementPage(-1));
+next?.addEventListener("click", () => incrementPage(1));
+getPage()
