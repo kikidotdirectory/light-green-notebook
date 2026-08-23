af977e3359090b6fe9d1dd282005d0ad697a36c4
kiki chang <inbox@kiki.directory>
2026-08-23 17:26:56 -0400

    refactor: simplify & clarify functions

--- src/js/page-navigation.ts
+++ src/js/page-navigation.ts
@@ -1,8 +1,8 @@
 declare const totalSpreads: number;
-
 let currentPageNum: number;
 
 const fileName = (page: number) => String(page).padStart(4, "0");
+const pageSrc = (pageNum: number) => "/assets/spreads/" + fileName(pageNum) + "." + fileExt;
 
 function getState(): number {
 	try {
@@ -17,26 +17,16 @@ function setState(pageNum: number) {
 	localStorage.setItem(notebookID, pageNum.toString());
 }
 
-function getPage() {
-	currentPageNum = getState();
-	replaceSpreadImage(currentPageNum);
-	notebookViewer.dataset.spread = currentPageNum.toString();
-}
-
-function numToPageSrc(pageNum: number) {
-	return "/assets/spreads/" + fileName(pageNum) + "." + fileExt;
-}
-
 function replaceSpreadImage(pageNum: number) {
 	const newImage = document.createElement("img");
 	newImage.className = spreadImage.className;
 	newImage.alt = spreadImage.alt;
-	newImage.src = numToPageSrc(pageNum);
+	newImage.src = pageSrc(pageNum);
 	spreadImage.replaceWith(newImage);
 	spreadImage = newImage;
 }
 
-function incrementPage(delta: number) {
+function flipPage(delta: number) {
 	const dest = currentPageNum + delta;
 	// prevent navigation from going outside bounds of notebook
 	if (dest < 0 || dest >= totalSpreads) return;
@@ -48,14 +38,14 @@ function incrementPage(delta: number) {
 	setState(currentPageNum);
 
 	// preload the next page in the current direction
-	if (currentPageNum !== 0 && currentPageNum !== totalSpreads) {
-		preloadPage(currentPageNum + delta);
-	}
+	preloadPage(currentPageNum + delta);
 }
 
 function preloadPage(pageNum: number) {
+	if (pageNum < 0) return;
+	if (pageNum >= totalSpreads) return;
 	const img = new Image();
-	img.src = numToPageSrc(pageNum);
+	img.src = pageSrc(pageNum);
 }
 
 // On page load, render notebook.
@@ -67,6 +57,12 @@ const prev = document.querySelector(".page-link.prev") as HTMLButtonElement;
 const next = document.querySelector(".page-link.next") as HTMLButtonElement;
 let spreadImage = document.querySelector(".spread-image") as HTMLImageElement;
 
-prev?.addEventListener("click", () => incrementPage(-1));
-next?.addEventListener("click", () => incrementPage(1));
-getPage()
+prev?.addEventListener("click", () => flipPage(-1));
+next?.addEventListener("click", () => flipPage(1));
+
+currentPageNum = getState();
+if (currentPageNum) {
+	replaceSpreadImage(currentPageNum);
+}
+preloadPage(currentPageNum - 1);
+preloadPage(currentPageNum + 1);
