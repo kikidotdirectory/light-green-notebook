9dc58c5fa7002ca5b9a680d7ca2990f531908507
kiki chang <inbox@kiki.directory>
2026-09-08 14:20:00 -0400

    implement subtitle rendering
    - iterates through an array per spread
    - switching spread changes or restarts the subtitles
    - subtitles stop iterating after finished

--- src/js/subtitles.ts
+++ src/js/subtitles.ts
@@ -1,6 +1,36 @@
 import { PageStore } from "./page-state.ts";
 
-export function initSubitles(pageStore: PageStore) {
-	const subtitleContainer = document.querySelector(".subtitles");
-	console.log(subtitleContainer);
+export function initSubtitles(pageStore: PageStore, el: HTMLElement) {
+	const subtitleContainer = el;
+	const delay = 4000; // 4s
+
+	const subtitles: Record<number, string[]> = {
+		0: [
+			"this website uses subtitles",
+			"you can click here to read them again if you miss them",
+			"the idea of website subtitles comes entirely from joseph pleass",
+			"thank you, joseph",
+		],
+	};
+
+	let index = 0;
+	let subtitleSetter: ReturnType<typeof setInterval> | undefined;
+
+	function showSubtitle(page: number) {
+		const subtitleContent = subtitles[page]?.[index];
+		if (!subtitleContent) {
+			if (subtitleSetter) clearInterval(subtitleSetter);
+			subtitleContainer.innerText = "";
+			return;
+		}
+		subtitleContainer.innerText = subtitleContent;
+		index++;
+	}
+
+	pageStore.subscribe((page) => {
+		if (subtitleSetter) clearInterval(subtitleSetter);
+		index = 0;
+		showSubtitle(page);
+		subtitleSetter = setInterval(() => showSubtitle(page), delay);
+	});
 }
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,11 @@
 import { initNotebook } from "./js/notebook-controller.ts";
 import { pageStore as storePageState } from "./js/page-state.ts";
-import { initSubitles } from "./js/subtitles.ts";
+import { initSubtitles } from "./js/subtitles.ts";
 import { initToc } from "./js/toc.ts";
 
 const pageState = storePageState();
 const toc = initToc(pageState);
 initNotebook(pageState, toc);
-initSubitles(pageState);
+
+const subtitle = document.querySelector(".subtitles");
+if (subtitle instanceof HTMLElement) initSubtitles(pageState, subtitle);
